c693b39439b87ed0da9b458115771b932847b745
Javier Castro <javiercastro.everis@latam.com>
2025-07-11 02:10:49 -0400

    update modificacion json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   image: {
     service: {
       entrypoint: 'astro/assets/services/noop'
